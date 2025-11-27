40ba92d52aa4ba1fa6e06ed68b07a082a93e05f4
Булат <4bxlia4@gmail.com>
2025-11-27 17:37:48 +0500

    Arrays DONE

--- Array/Program.cs
+++ Array/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +9,10 @@ namespace Array
 {
     internal class Program
     {
-        static void Main(string[] args)
+        const string delimeter = "\n------------------------------";
+        /*static void Main(string[] args)
         {
+        #if ARRAYS1
             int[] arr = { 3, 5, 8, 13, 21 };
             for (int i = 0; i < arr.Length; i++)
             {
@@ -36,7 +39,54 @@ namespace Array
             Console.WriteLine($"Минимальный элемент: {min1}");
             Console.WriteLine($"Максимальный элемент: {max1}");
 
-            Console.ReadKey();
+            Console.ReadKey();*/
+
+        static void Main()
+        {
+            Random random = new Random();
+
+            Console.Write("Введите количество строк: ");
+            int rows = int.Parse(Console.ReadLine());
+
+            Console.Write("Введите количество столбцов: ");
+            int columns = int.Parse(Console.ReadLine());
+
+            int[,] array = new int[rows, columns];
+
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    array[i, j] = random.Next(1, 101); 
+                }
+            }
+
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    Console.Write(array[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+
+            int sum = 0;
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    sum += array[i, j];
+                }
+            }
+
+            Console.WriteLine($"\nСумма элементов: {sum}");
+            Console.WriteLine($"\nСреднее-арифметическое элементов: {(double)sum/array.Length}");
+            Console.WriteLine(delimeter);
+            Console.WriteLine($"\nСумма элементов: {array.Cast<int>().Sum()}");
+            Console.WriteLine($"\nМинимальное значение: {array.Cast<int>().Min()}");
+            Console.WriteLine($"\nМаксимальное значение: {array.Cast<int>().Max()}");
+
         }
     }
 }
+
